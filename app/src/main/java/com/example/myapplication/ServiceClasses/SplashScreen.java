5f2a966f1754f1a2b34377c4e8b9eacc2d247a65
Maxim Gorbachevskiy 750503 <ceyz@list.ru>
2020-04-06 14:22:18 +0300

    feat: fix for buttons for navigative drawer

--- app/src/main/java/com/example/myapplication/ServiceClasses/SplashScreen.java
+++ app/src/main/java/com/example/myapplication/ServiceClasses/SplashScreen.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.myapplication.R;
 
@@ -14,11 +16,15 @@ public class SplashScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.splash_screen);
+        Window w= getWindow();
+
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent mainIntent = new Intent(SplashScreen.this, DrawerActivity.class);
                 SplashScreen.this.startActivity(mainIntent);
                 SplashScreen.this.finish();
             }
