bb6cb01b69a503514097ebde9a9f8018ae4b96f1
Kapitanov Nikita 750503 <kapnik3750@gmail.com>
2020-04-06 11:39:52 +0300

    Removed swiper from project, left buttons for another variant of swipe

--- src/js/command.js
+++ src/js/command.js
@@ -1,21 +1,3 @@
-var mySwiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-})
-
 function showSwiper() {
     let close = document.getElementById("swiper-container");
     let lefbar = document.getElementById("menu");
